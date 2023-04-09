fd7ad56f5439f5121af380220c5b6cf35ed5b33f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-09 20:48:12 +0300

    Add grabbing a previous Element

---
+++ JS/Detail_Popup.js
@@ -0,0 +1,4 @@
+/* Here I'm grabbing a previous Element,
+so I can reference it in a later function, and being able to insert
+a new Generated element after this PREVIOUS_NODE.  */
+const PREVIOUS_NODE = document.getElementById('headline-container');
