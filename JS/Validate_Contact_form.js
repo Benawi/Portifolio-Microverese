c2bc9a1e567318a12aebc0f5f8eb221e8fb681fd
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-10 23:55:32 +0300

    Add action litsener for submit button

--- JS/Validate_Contact_form.js
+++ JS/Validate_Contact_form.js
@@ -1,1 +1,12 @@
 const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    const validateRegex = /[A-Z]/;
+  
+    if (validateRegex.test(emailInput.value)) {
+      document.getElementById('email_message').classList.toggle('disabled');
+      event.preventDefault();
+    } else if (!document.getElementById('email_message').className === 'email-message disabled') {
+      document.getElementById('email_message').classList.toggle('disabled');
+    }
+  });
