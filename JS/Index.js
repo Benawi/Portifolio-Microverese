2206af5e5d7c20b619a70d2b928a65041bc2ac1a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-10 18:39:29 +0300

    Comment bug fixed

--- JS/Index.js
+++ JS/Index.js
@@ -4,13 +4,6 @@ const MOBILE_EXPANDED_MENU = document.getElementById("mobile-expanded-menu");
 const MENU_NODES = [MOBILE_DROP_DOWN_BTN, MOBILE_EXPANDED_MENU];
 
 // Event listeners to open and close the mobile menu dropdown
-
-// for (let i = 0; i < MENU_NODES.length; i += 1) {
-//   MENU_NODES[i].addEventListener('click', () => {
-//     document.getElementById('mobile-expanded-menu').classList.toggle('disabled');
-//     document.querySelector('html').classList.toggle('no-overflow-y');
-//   });
-// }
 // Amin add this as coding partner
 MENU_NODES.map((menuNode) => {
   menuNode.addEventListener("click", () => {
