b74796ce0be401dad8f74c42af78dc21c62789e8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-11 12:12:55 +0300

    Add regular expression for email validation

--- JS/Validate_Contact_form.js
+++ JS/Validate_Contact_form.js
@@ -1,9 +1,12 @@
+/* The below code is selecting the form element from the DOM. */
 const form = document.querySelector('form');
-
+const emailInput = document.getElementById('user_email');
 form.addEventListener('submit', (event) => {
-    const validateRegex = /[A-Z]/;
+    /* The below code is a regular expression that checks if the email is valid. */
+const emailRegex = /[A-Z]/;
+   
   
-    if (validateRegex.test(emailInput.value)) {
+    if (emailRegex.test(emailInput.value)) {
       document.getElementById('email_message').classList.toggle('disabled');
       event.preventDefault();
     } else if (!document.getElementById('email_message').className === 'email-message disabled') {
