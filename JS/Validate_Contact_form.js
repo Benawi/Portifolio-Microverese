4c831fffc043aacc8814fb1dc3ee7eb0d216be02
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 13:38:47 +0300

    Regular expression add for email validation

--- JS/Validate_Contact_form.js
+++ JS/Validate_Contact_form.js
@@ -1,6 +1,7 @@
 /* The below code is selecting the form element from the DOM. */
 const form = document.querySelector('form');
 const emailInput = document.getElementById('user_email');
+
 form.addEventListener('submit', (event) => {
  /* The below code is a regular expression that checks if the email is valid. */
 const emailRegex = /[A-Z]/;
@@ -8,6 +9,6 @@ const emailRegex = /[A-Z]/;
   /* This code added by the code partener */
 
 
-  
+
 
 });
