0729987bb865fc94b73f97b2c3754ce63251cbee
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 16:51:14 +0300

    Get values of form elements

--- JS/Preserve_Data_Browser.js
+++ JS/Preserve_Data_Browser.js
@@ -4,3 +4,14 @@
  const dataMessage = document.getElementById('user_message');
  /* store  elements to an array  */
 const values = [dataName, dataEmail, dataMessage];
+
+/* Get values of elements  */
+function allData() {
+    const obj = {};
+  
+    for (let i = 0; i < values.length; i += 1) {
+      obj[values[i].id] = values[i].value;
+    }
+  
+    return obj;
+  }
