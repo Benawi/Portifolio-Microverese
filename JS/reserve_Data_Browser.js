2f5e6704a93215e30783f6e6322e87b089a441b2
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 16:47:58 +0300

    Get all form elements

---
+++ JS/reserve_Data_Browser.js
@@ -0,0 +1,4 @@
+ /* Get all form components/components */
+ const dataName = document.getElementById('user_name');
+ const dataEmail = document.getElementById('user_email');
+ const dataMessage = document.getElementById('user_message');
