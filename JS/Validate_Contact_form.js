aacda322cc6e36bcea73ea95a188fdb35d443e2d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 16:37:16 +0300

    Javascript linters bugs fixed

--- JS/Validate_Contact_form.js
+++ JS/Validate_Contact_form.js
@@ -3,13 +3,13 @@ const form = document.querySelector('form');
 const emailInput = document.getElementById('user_email');
 
 form.addEventListener('submit', (event) => {
- /* The below code is a regular expression that checks if the email is valid. */
-const emailRegex = /[A-Z]/;
-// Amin writes this code
-    if (emailRegex.test(emailInput.value)) {
-      document.getElementById('email_message').classList.toggle('disabled');
-      event.preventDefault();
-    } else if (!document.getElementById('email_message').className === 'email-message disabled') {
-      document.getElementById('email_message').classList.toggle('disabled');
-    }
+  /* The below code is a regular expression that checks if the email is valid. */
+  const emailRegex = /[A-Z]/;
+  // Amin writes this code
+  if (emailRegex.test(emailInput.value)) {
+    document.getElementById('email_message').classList.toggle('disabled');
+    event.preventDefault();
+  } else if (!document.getElementById('email_message').className === 'email-message disabled') {
+    document.getElementById('email_message').classList.toggle('disabled');
+  }
 });
