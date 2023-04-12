dd39707ab9af4fbeff3c8fda74fb70f55e7d5272
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 20:45:32 +0300

    Add comment to Presever_data js file

--- JS/Detail_Popup.js
+++ JS/Detail_Popup.js
@@ -486,7 +486,7 @@ function buildModalPopUp(button, title, Img) {
 
   modalText.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.";
 
-  if (window.innerWidth >= 1100) {
+  if (window.innerWidth >= 400) {
     modalCloseBtn.setAttribute(
       'src',
       './css/img/detailed_works/icon_close_desk.svg',
