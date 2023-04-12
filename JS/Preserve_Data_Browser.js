8e60e26e028fe13a86047650ff986b80131c2adb
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-12 16:49:18 +0300

    store  elements to an array

--- JS/Preserve_Data_Browser.js
+++ JS/Preserve_Data_Browser.js
@@ -2,3 +2,5 @@
  const dataName = document.getElementById('user_name');
  const dataEmail = document.getElementById('user_email');
  const dataMessage = document.getElementById('user_message');
+ /* store  elements to an array  */
+const values = [dataName, dataEmail, dataMessage];
